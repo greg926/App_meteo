78ae76b1d90b671afea50eaa8d44479eabe5d0e2
Grégory Barboza <greg.barboza@gmail.com>
2021-12-09 12:10:52 +0100

    1er appel à l'API

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+const CLEFAPI = 'd5879cc9e423f0d25d27a62bd5c6ddb1';
+let resultatsAPI;
+
+// Géolocalisation
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+
+        //console.log(position);
+        let long = position.coords.longitude;
+        let lat = position.coords.latitude;
+        AppelAPI(long, lat);
+
+    }, () => {
+        alert(`Vous avez refusé la géolocalisation, l'application ne peut pas fonctionner, veuiller l'activer`)
+    })
+
+}
+
+function AppelAPI(long, lat) {
+    
+    //console.log(long, lat);
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`)
+    .then((reponse) => {
+        return reponse.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+
+}
