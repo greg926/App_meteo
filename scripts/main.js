5b232c36208cc4b1cbd5c941cc03028cfca89c43
Grégory Barboza <greg.barboza@gmail.com>
2021-12-09 14:02:08 +0100

    Prévisions des jours + loader

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import tabJoursEnOrdre from './Utilitaire/gestionTemps.js';
 
-console.log("DEPUIS MAIN JS:" + tabJoursEnOrdre);
+//console.log("DEPUIS MAIN JS:" + tabJoursEnOrdre);
 
 const CLEFAPI = 'd5879cc9e423f0d25d27a62bd5c6ddb1';
 let resultatsAPI;
@@ -10,6 +10,10 @@ const temperature = document.querySelector('.temperature');
 const localisation = document.querySelector('.localisation');
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+const joursDiv = document.querySelectorAll('.jour-prevision-nom');
+const tempJoursDiv = document.querySelectorAll('.jour-prevision-temp');
+const imgIcone = document.querySelector('.logo-meteo');
+const chargementContainer = document.querySelector('.overlay-icone-chargement');
 
 // Géolocalisation
 if(navigator.geolocation) {
@@ -34,11 +38,11 @@ function AppelAPI(long, lat) {
         return reponse.json();
     })
     .then((data) => {
-        console.log(data);
+        //console.log(data);
 
         resultatsAPI = data;
         temps.innerText = resultatsAPI.current.weather[0].description;
-        temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}° `;
+        temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}° `
         localisation.innerText = resultatsAPI.timezone;
 
 
@@ -60,9 +64,30 @@ function AppelAPI(long, lat) {
 
         // Temp pour 3h
         for(let j = 0; j < tempPourH.length; j++) {
-            tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`;
+            tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`
         }
 
+        // Trois premières lettres des jours 
+
+        for(let k = 0; k < tabJoursEnOrdre.length; k++) {
+            joursDiv[k].innerText = tabJoursEnOrdre[k].slice(0,3);
+        }
+
+
+        // Température par jour
+        for(let m = 0; m < 7; m++) {
+            tempJoursDiv[m].innerText = `${Math.trunc(resultatsAPI.daily[m + 1].temp.day)}°`
+        }
+
+
+        // Icone dynamique
+        if(heureActuelle >= 6 && heureActuelle < 21) {
+            imgIcone.src = `ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`
+        }else {
+            imgIcone.src = `ressources/nuit/${resultatsAPI.current.weather[0].icon}.svg`
+        }
+
+        chargementContainer.classList.add('disparation')
 
     })
 
